e57cbb8de091ecaf20d9db510ded8798496053da
ImNotGabo <gabriel.duarte18@outlook.es>
2025-01-01 19:12:07 -0400

    Add ICON_VARIANTS utility for animated icon effects

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -14,3 +14,16 @@ export const CHILD_VARIANT = {
 	hidden: { opacity: 0, x: -100 },
 	visible: { opacity: 1, x: 0, transition: { duration: 0.3 } },
 };
+
+export const ICON_VARIANTS = (duration) => ({
+	initial: { y: -10 },
+	animate: {
+		y: [10, -10],
+		transition: {
+			duration: duration,
+			ease: 'linear',
+			repeat: Infinity,
+			repeatType: 'reverse',
+		},
+	},
+});
