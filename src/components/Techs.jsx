1f308dbdf7e1c364f650aae7ea7276104bd5f1aa
ImNotGabo <gabriel.duarte18@outlook.es>
2025-01-01 19:12:11 -0400

    Enhance Techs component with framer-motion for animated technology icons

--- src/components/Techs.jsx
+++ src/components/Techs.jsx
@@ -7,40 +7,99 @@ import {
 	SiRedux,
 	SiTypescript,
 } from 'react-icons/si';
+import { ICON_VARIANTS } from '../utils/utils';
+import { motion } from 'framer-motion';
 
 const Techs = () => {
 	return (
 		<section className='pb-24'>
-			<h2 className='my-20 text-center text-4xl'>Technologies</h2>
-			<div className='flex flex-wrap items-center justify-center gap-4'>
-				<div className='p-4'>
+			<motion.h2
+				className='my-20 text-center text-4xl'
+				whileInView={{ opacity: 1, y: 0 }}
+				initial={{ opacity: 0, y: -100 }}
+				transition={{ duration: 1.2 }}
+			>
+				Technologies
+			</motion.h2>
+			<motion.div
+				className='flex flex-wrap items-center justify-center gap-4'
+				whileInView={{ opacity: 1, x: 0 }}
+				initial={{ opacity: 0, x: -100 }}
+				transition={{ duration: 1.2 }}
+			>
+				<motion.div
+					className='p-4'
+					initial='initial'
+					animate='animate'
+					variants={ICON_VARIANTS(1.5)}
+				>
 					<SiJavascript className='text-6xl text-[#F7DF1E]' />
-				</div>
-				<div className='p-4'>
+				</motion.div>
+				<motion.div
+					className='p-4'
+					initial='initial'
+					animate='animate'
+					variants={ICON_VARIANTS(2)}
+				>
 					<SiTypescript className='text-6xl text-[#3178C6]' />
-				</div>
-				<div className='p-4'>
+				</motion.div>
+				<motion.div
+					className='p-4'
+					initial='initial'
+					animate='animate'
+					variants={ICON_VARIANTS(2.5)}
+				>
 					<RiReactjsFill className='text-7xl text-[#61DAFB]' />
-				</div>
-				<div className='p-4'>
+				</motion.div>
+				<motion.div
+					className='p-4'
+					initial='initial'
+					animate='animate'
+					variants={ICON_VARIANTS(3)}
+				>
 					<SiRedux className='text-6xl text-[#764ABC]' />
-				</div>
-				<div className='p-4'>
+				</motion.div>
+				<motion.div
+					className='p-4'
+					initial='initial'
+					animate='animate'
+					variants={ICON_VARIANTS(3.5)}
+				>
 					<FaNodeJs className='text-6xl text-[#339933]' />
-				</div>
-				<div className='p-4'>
+				</motion.div>
+				<motion.div
+					className='p-4'
+					initial='initial'
+					animate='animate'
+					variants={ICON_VARIANTS(3)}
+				>
 					<SiExpress className='text-6xl text-[#fff]' />
-				</div>
-				<div className='p-4'>
+				</motion.div>
+				<motion.div
+					className='p-4'
+					initial='initial'
+					animate='animate'
+					variants={ICON_VARIANTS(2.5)}
+				>
 					<SiMongodb className='text-5xl text-[#47A248]' />
-				</div>
-				<div className='p-4'>
+				</motion.div>
+				<motion.div
+					className='p-4'
+					initial='initial'
+					animate='animate'
+					variants={ICON_VARIANTS(2)}
+				>
 					<RiTailwindCssFill className='text-6xl text-[#06B6D4]' />
-				</div>
-				<div className='p-4'>
+				</motion.div>
+				<motion.div
+					className='p-4'
+					initial='initial'
+					animate='animate'
+					variants={ICON_VARIANTS(1.5)}
+				>
 					<FaGitAlt className='text-6xl text-[#F05033]' />
-				</div>
-			</div>
+				</motion.div>
+			</motion.div>
 		</section>
 	);
 };
