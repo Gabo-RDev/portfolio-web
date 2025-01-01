0fc746e5fae5be5e55ca3706e9fff299c4b5ad2d
ImNotGabo <gabriel.duarte18@outlook.es>
2025-01-01 19:39:06 -0400

    Enhance Contact component with framer-motion for improved animations

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,34 @@
 import { CONTACT } from '../constants/index';
+import { motion } from 'framer-motion';
 
 const Contact = () => {
 	return (
 		<footer className='border-t border-stone-900 pb-20'>
-			<h2 className='my-10 text-center text-4xl'>Get in touch</h2>
+			<motion.h2
+				className='my-10 text-center text-4xl'
+				whileInView={{ opacity: 1, y: 0 }}
+				initial={{ opacity: 1, y: -100 }}
+				transition={{ duration: 0.5 }}
+			>
+				Get in touch
+			</motion.h2>
 			<div className='text-center tracking-tighter'>
-				<p className='my-4'>{CONTACT.address}</p>
-				<p className='my-4'>{CONTACT.phoneNo}</p>
+				<motion.p
+					className='my-4'
+					whileInView={{ opacity: 1, x: 0 }}
+					initial={{ opacity: 0, x: -100 }}
+					transition={{ duration: 1 }}
+				>
+					{CONTACT.address}
+				</motion.p>
+				<motion.p
+					className='my-4'
+					whileInView={{ opacity: 1, x: 0 }}
+					initial={{ opacity: 0, x: -100 }}
+					transition={{ duration: 1 }}
+				>
+					{CONTACT.phoneNo}
+				</motion.p>
 				<a
 					href={`mailto:${CONTACT.email}`}
 					className='border-b'
