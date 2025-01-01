1a162ed4384cfaa06506e06bb3a960ef77c91320
ImNotGabo <gabriel.duarte18@outlook.es>
2025-01-01 18:28:12 -0400

    Enhance Contact component with structured footer layout and dynamic contact information

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,19 @@
+import { CONTACT } from '../constants/index';
+
 const Contact = () => {
 	return (
-		<footer>
-			<div> </div>
+		<footer className='border-t border-stone-900 pb-20'>
+			<h2 className='my-10 text-center text-4xl'>Get in touch</h2>
+			<div className='text-center tracking-tighter'>
+				<p className='my-4'>{CONTACT.address}</p>
+				<p className='my-4'>{CONTACT.phoneNo}</p>
+				<a
+					href={`mailto:${CONTACT.email}`}
+					className='border-b'
+				>
+					{CONTACT.email}
+				</a>
+			</div>
 		</footer>
 	);
 };
