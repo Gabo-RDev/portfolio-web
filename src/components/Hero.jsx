534368198c6f824a235a8713ed8c8f14b9d88158
ImNotGabo <gabriel.duarte18@outlook.es>
2025-01-09 18:36:58 -0400

    Update resume link in Hero component to point to the correct asset path

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,7 +48,7 @@ const Hero = () => {
 						</motion.p>
 						<motion.a
 							className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'
-							href='/resume.pdf'
+							href='../assets/resume.pdf'
 							target='_blank'
 							rel='noopener noreferrer'
 							download
