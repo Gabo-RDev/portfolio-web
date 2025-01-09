165ef04e7aef451724914f11e4337204c2820be9
ImNotGabo <gabriel.duarte18@outlook.es>
2025-01-09 14:58:46 -0400

    Comment out unused motion elements in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,22 +13,22 @@ const Contact = () => {
 				Get in touch
 			</motion.h2>
 			<div className='text-center tracking-tighter'>
-				<motion.p
+				{/* <motion.p
 					className='my-4'
 					whileInView={{ opacity: 1, x: 0 }}
 					initial={{ opacity: 0, x: -100 }}
 					transition={{ duration: 1 }}
 				>
 					{CONTACT.address}
-				</motion.p>
-				<motion.p
+				</motion.p> */}
+				{/* <motion.p
 					className='my-4'
 					whileInView={{ opacity: 1, x: 0 }}
 					initial={{ opacity: 0, x: -100 }}
 					transition={{ duration: 1 }}
 				>
 					{CONTACT.phoneNo}
-				</motion.p>
+				</motion.p> */}
 				<a
 					href={`mailto:${CONTACT.email}`}
 					className='border-b'
